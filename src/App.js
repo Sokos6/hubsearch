e503711d1e3e1715838e7cfc0e5b488e4a2b714b
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-26 16:18:41 -0500

    fetching repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [keyword, setKeyword] = useState([]);
+
+  const fetchData = () => {
+    const url = `https://api.github.com/search/repositories?q=${keyword}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData.items);
+      });
+  };
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const tableRows = data.map((item, index) => (
+    <tr key={index}>
+      <td>{item.full_name}</td>
+      <td>
+        <a href={item.html_url}>{item.html_url}</a>
+      </td>
+    </tr>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='App'>
+    <input type="text" onChange={handleChange} />
+    <button onClick={fetchData} value={keyword}>FETCH</button>
+  <table><tbody>{tableRows}</tbody></table>
+  </div>
   );
 }
 
